5be19f643cd6e96e3ec21a4b8b5b8055cd16deaf
Andrew Dinh <adinh@sightmachine.com>
2019-04-11 09:24:37 -0700

    Enable password input filling in

--- test/upwork/upwork_login.spec.js
+++ test/upwork/upwork_login.spec.js
@@ -15,21 +15,21 @@ describe('Tests for Upwork Login functionalities', async () => {
   });
 
   it('should login to home page', async () => { /* simple test case */
-    const emailInput = "#login_username";
-    const passwordInput = "#login-password";
+    const userInput = "#login_username";
+    const passwordInput = "#login_password";
     const submitSelector = "#login-submit";
 
-    linkEmail = await page.$(emailInput);
-    await linkEmail.click({ clickCount: 3 });
-    await linkEmail.type('testing@email.com'); // add the email address for linkedin //
+    usernameInput = await page.$(userInput);
+    await usernameInput.click({ clickCount: 3 });
+    await usernameInput.type(process.env.UPWORK_USER);   // add the user name for Upwork
 
-    // linkPassword = await page.$(passwordInput);
+    linkPassword = await page.$(passwordInput);
+    await linkPassword.click({ clickCount: 3 });
+    await linkPassword.type(process.env.LINKEDIN_PASSWORD); // add password for linkedin account
+    
+    
+    
     // linkSubmit = await page.$(submitSelector);
-
-
-    // await linkPassword.click({ clickCount: 3 });
-    // await linkPassword.type(process.env.LINKEDIN_PASSWORD); // add password for linkedin account
-
     // await linkSubmit.click();
     // await page.waitFor(3000);
   });
