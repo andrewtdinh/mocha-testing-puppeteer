3d6b1d55a214eaeefc51e362bf5ac038e2ad2d26
Andrew Dinh <adinh@sightmachine.com>
2019-04-20 23:26:30 -0700

    Can take screenshot but not on error

--- test/upwork/upwork_login.spec.js
+++ test/upwork/upwork_login.spec.js
@@ -9,25 +9,25 @@ describe('Tests for Upwork Login functionalities', async () => {
     await page.goto("https://www.upwork.com/ab/account-security/login");
     await page.setViewport({ width: 1920, height: 1040 });
     page.on('error', async () => {
-      await page.screenshot({ path: './error.png' });
     })
   });
-
+  
   after(async function () { /* after hook for mocha testing */
     await page.close();
   });
-
+  
   it('should login to home page', async () => { /* simple test case */
     const userInput = '#login_username';
     const continueBtn = 'button[type="submit"]'
     const passwordInput = '#login_password';
     const submitSelector = '#login-submit';
-
+    
     usernameInput = await page.$(userInput);
     await usernameInput.click({ clickCount: 3 });
     await usernameInput.type(process.env.UPWORK_USER);   // add the user name for Upwork account
     continueButton = await page.$(continueBtn);
     await continueButton.click();
+    await page.screenshot({ path: './test.png' });
 
     pwInputField = await page.$(passwordInput);
     await pwInputField.click({ clickCount: 3 });
